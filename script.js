7daa275782286cd23c65a5f9d85825b441a5de17
Calvin Roberts <roberts.calvin85@gmail.com>
2022-10-04 22:24:21 +0100

    Create function to remove active class on link

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 'use strict';
+let link = document.getElementsByClassName('link');
+
+let currentValue = 1;
+
+function activeLink() {
+  for (l of link) {
+    l.classList.remove('active');
+  }
+}
