cbac173f2818b30d7d86762a9a4cf048d9b8253c
Calvin Roberts <roberts.calvin85@gmail.com>
2022-10-04 22:44:44 +0100

    Add functions for next and back buttons

--- script.js
+++ script.js
@@ -9,3 +9,23 @@ function activeLink() {
   event.target.classList.add('active');
   currentValue = event.target.value;
 }
+
+function backBtn() {
+  if (currentValue > 1) {
+    for (l of link) {
+      l.classList.remove('active');
+    }
+    currentValue--;
+    link[currentValue - 1].classList.add('active');
+  }
+}
+
+function nextBtn() {
+  if (currentValue < 6) {
+    for (l of link) {
+      l.classList.remove('active');
+    }
+    currentValue++;
+    link[currentValue - 1].classList.add('active');
+  }
+}
